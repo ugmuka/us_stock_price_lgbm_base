9e0f306a7bcab05dc964f00f8579fabd101f6368
muka-1206 <mukai_e_equal_2.718@outlook.jp>
2021-11-15 17:51:16 +0900

    [fix] デバッグ用のコードが残っていた

--- runner.py
+++ runner.py
@@ -20,12 +20,6 @@ class Runner:
         # preprocess
         preprocessor = Preprocessor(dataset_df, export_path_preprocess)
         df = preprocessor.preprocess(update=True)
-        # col = ['Year','Month','Day','WeekOfYear','IPOyear','Sector','Industry','List1','List2','lag_1_y_diff_norm','lag_2_y_diff_norm','lag_3_y_diff_norm','lag_4_y_diff_norm','rmean_1_y_diff_norm','rstd_1_y_diff_norm']
-        # train_df_sort = df.sort_values(by=['Year','Month','Day'])
-        # x_train_sort = train_df_sort[col]
-        # y_train_sort = train_df_sort['y_diff_norm']
-        # x_train_sort.to_csv('preprocess_x.csv', index=False)
-        # y_train_sort.to_csv('preprocess_y.csv', index=False)
         
         # train model
         config_path = './config/lgbm_params.yml'
